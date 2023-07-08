7c04256e47290aea0ff85a601c20e27c1ac23e2e
JanviB16 <117053849+JanviB16@users.noreply.github.com>
2023-07-07 23:16:13 -0400

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace omas.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
